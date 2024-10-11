14bf0c663a07088cce195e078c9f1371f438412f
Emelie Nyberg <emelie.kedert@gmail.com>
2024-10-11 12:30:11 +0200

    added comments and a logo picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+// Import data from json file
 import data from "./data.json";
+
+// Import header and album card components
 import { Header } from "./components/Header";
 import { AlbumCard } from "./components/AlbumCard";
-
-console.log(data.albums.items);
 
 export const App = () => {
   return (
     <>
+      {/* Add header component */}
       <Header />
+
       <main className="album-card-container">
+        {/* Map items from items in the json data, naming it albumcard */}
         {data.albums.items.map((albumcard) => (
-          <AlbumCard key={albumcard.id} albumcard={albumcard} />
+          < AlbumCard key={albumcard.id} albumcard={albumcard} />
         ))}
       </main>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
+// Import Melodify logo from assets folder
+import MelodifyLogo from "../assets/melodify_logo.png"
+
 export const Header = () => {
-    return (
+  return (
 
-        <header>
-            <h1>melodify</h1>
-            <button className="sign-in-btn">Sign in</button>
-        </header>
+    <header>
+      <div className="logo">
+        <h1>Melodify</h1>
 
+        <img src={MelodifyLogo} alt="Melodify logo" />
+      </div>
 
-
-    );
+      <button className="log-out-btn">Log out</button>
+    </header>
+  );
 };
